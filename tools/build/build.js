6d53e3d2525e0a9d2c2fdb831b1f622e1a268284
kyunkyunkyun <maxkrutoda0@gmail.com>
2026-05-19 01:35:50 +0500

    Update build.js

--- tools/build/build.js
+++ tools/build/build.js
@@ -55,7 +55,15 @@ export const NoWarningParameter = new Juke.Parameter({
 export const DmMapsIncludeTarget = new Juke.Target({
   executes: async () => {
     const folders = [
-      ...Juke.glob("_maps/**/*.dmm"),
+      ...Juke.glob("_maps/outpost/**/*.dmm"),
+      // [CELADON-EDIT] - CELADON_CONFIGS_MAPS
+      // ...Juke.glob("_maps/RandomRuins/**/*.dmm"),
+      // ...Juke.glob("_maps/shuttles/**/*.dmm"), // CELADON-EDIT - ORIGINAL
+      ...Juke.glob("_maps/_mod_celadon/RandomRuins/**/*.dmm"),
+      ...Juke.glob("_maps/_mod_celadon/shuttles/**/*.dmm"),
+      ...Juke.glob("_maps/**/runtime_check_map.dmm"),
+      // [/CELADON-EDIT]
+      ...Juke.glob("_maps/templates/**/*.dmm"),
     ];
     const content =
       folders
